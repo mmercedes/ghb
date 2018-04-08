5d49068635397fe3f1d3d65eaf0ea8cf07a5211a
mmercedes <matthewmercedes@gmail.com>
2018-04-07 23:17:00 -0400

    Brew formula update for ghb version v0.1.0

--- ghb.rb
+++ ghb.rb
@@ -3,7 +3,7 @@ class Ghb < Formula
   homepage ""
   url "https://github.com/mmercedes/ghb/releases/download/v0.1.0/ghb_0.1.0_darwin_amd64.tar.gz"
   version "0.1.0"
-  sha256 "f4b08d74c1782a45e2d1a57c921ec6c4756ed71e8e572a7cf3cbc7d77b975667"
+  sha256 "999d6994b8efaa1d2abd166821e67e6943694b5d91b9cf0663256b360d42a9b8"
 
   def install
     bin.install "ghb"
