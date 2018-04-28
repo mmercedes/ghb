233314948cc2f46299c0505c8f0644c1f92ae303
mmercedes <matthewmercedes@gmail.com>
2018-04-28 18:39:22 -0400

    Brew formula update for ghb version v0.1.1

--- ghb.rb
+++ ghb.rb
@@ -1,9 +1,9 @@
 class Ghb < Formula
   desc "A tool for performing backups and deletions of Github gists and starred repositories"
   homepage ""
-  url "https://github.com/mmercedes/ghb/releases/download/v0.1.0/ghb_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "99d70bafb74acd979a73a3b0ebe075d257ba92b01f2ad55128d86d59ffad506d"
+  url "https://github.com/mmercedes/ghb/releases/download/v0.1.1/ghb_0.1.1_darwin_amd64.tar.gz"
+  version "0.1.1"
+  sha256 "44d48173abc55bf577dc9a6a710a2387486ac494c562d8548da24106448c15bd"
 
   def install
     bin.install "ghb"
